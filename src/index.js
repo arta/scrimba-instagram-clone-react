ca1ad6166de250d17ed519cea6528bb430dc843d
Martin Fencl <martin@layeredcode.com>
2021-02-25 13:12:39 -0600

    Mimic Karl's base set up; remove & comment out some stuff
    Tried to match Karl's setup as close as possible, images not downloaded yet (won't they be in the firebase?).
    Pulling current CDN versions of Tailwind and Firebase; just as with all of our npm installed & packaged dependencies: react, react-dom, ..
    Looks good, after some trial & error everything runs.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <h1 className="text-red-500">Hello, World</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
