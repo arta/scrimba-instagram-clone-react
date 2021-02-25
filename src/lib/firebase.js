52c742ace846aa462db385f9a9d206a2b063b34c
Martin Fencl <martin@layeredcode.com>
2021-02-25 16:09:09 -0600

    import Firebase into app, remove config from gitignore, use env variables
    Realized the config must be on the server (remote repo) for the app to work. :)
    So, remove hard coded sensitive info from the config file and move it to environment variables.

---
+++ src/lib/firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "scrimba-instagram-clone-react.firebaseapp.com",
+  projectId: "scrimba-instagram-clone-react",
+  storageBucket: "scrimba-instagram-clone-react.appspot.com",
+  messagingSenderId: process.env.FIREBASE_MS_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+// www.gstatic.com/firebasejs/8.2.9/firebase-app.js pulled in on index.html
+// declares a firebase global variable (on window)
+// so, we can now write window.firebase...
+const Firebase = window.firebase.initializeApp(firebaseConfig);
+
+export { Firebase };
