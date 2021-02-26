61e5c1bf822aed1523fff52a1daceb0871434b22
Martin Fencl <martin@layeredcode.com>
2021-02-26 16:33:06 -0600

    Seed firebase w data, implement env variables
    Do not push sensitive info to github. This took a while to figure all out w React. Huge dev't note in /src/lib/firebase.js. Environmental variables used in React must start w/ `REACT_APP_` to be accessible via process.env.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,16 +1,57 @@
+// #############################################################################
+// 2-26-21: ran into a cascading & interlaced issues here
+// From top to bottom of it all:
+
+// 1) Immediate error: CORS. Err message:
+// "Cross-Origin Request Blocked: The Same Origin Policy
+// disallows reading the remote resource at
+// https://www.gstatic.com/firebasejs/8.2.9/firebase-firestore.js.
+// (Reason: CORS header ‘Access-Control-Allow-Origin’ missing)."
+// I spent a LOT of time trying to resolve unsuccessfully
+
+// 2) To discover this was not CORS issue.
+// This was caused by invalid data send to Firebase. Some post somewhere made
+// me realize this. Then I paid attention to this Err message:
+// "Uncaught FirebaseError: Function addDoc() called with invalid data.
+// Unsupported field value: undefined (found in field userId in document
+// users/g6ZGQFwYQA9Zqv3niyjK)"
+
+// 2.1) This I suspected to be a possible source of problems all along.
+// Caused by environment variables not being accessed from React app JS.
+// Turns out, React solves this; env variable must start with `REACT_APP_`
+// https://create-react-app.dev/docs/adding-custom-environment-variables/
+// But fixing that took me to yet another, kinda "root" cause of it all
+
+// 2.2) The "root" cause regardless of all of the above. Err message:
+// "Uncaught (in promise) FirebaseError: Missing or insufficient permissions."
+// That's right, the permissions set in firebase console require being logged in
+// to write data (which seeding a databse is).
+// FIX: temporarily (for firebase seeding only), make authentication rules (in
+// the firebase console project authenticaiton setting):
+// "allow read, write;"
+// then - right after seeding - turn the rules back to:
+// "allow read; allow write: if request.auth.uid != null;"
+// #############################################################################
+
+
+
+// import { seedDatabase } from '../seed'; // run only 1x, for db seeding
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "scrimba-instagram-clone-react.firebaseapp.com",
   projectId: "scrimba-instagram-clone-react",
   storageBucket: "scrimba-instagram-clone-react.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MS_ID,
-  appId: process.env.FIREBASE_APP_ID
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MS_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // www.gstatic.com/firebasejs/8.2.9/firebase-app.js pulled in on index.html
 // declares a firebase global variable (on window)
 // so, we can now write window.firebase...
 const FirebaseApp = window.firebase.initializeApp(firebaseConfig);
-const { FieldValue } = FirebaseApp.firestore; // Karl: window.firebase.firestore;
+const { FieldValue } = FirebaseApp.firestore; // Karl: window.firebase.firestore
+
+// seedDatabase(FirebaseApp); // run only 1x, for db seeding
 
 export { FirebaseApp, FieldValue };
---
+++ src/seed.js
@@ -0,0 +1,75 @@
+// NOTE: replace 'bmjpkU8bBWRse2JwKgzOGTv5tUI3' with your
+// Firebase auth user id (can be taken from Firebase at the auth section!
+// Look for User UID)
+
+export function seedDatabase(FirebaseApp) {
+  const users = [
+    {
+      userId: process.env.REACT_APP_FIREBASE_USER_ID,
+      username: 'arta',
+      fullName: 'Arta Parta',
+      emailAddress: 'artaparta@layeredcode.com',
+      following: ['2'],
+      followers: ['2', '3', '4'],
+      dateCreated: Date.now()
+    },
+    {
+      userId: '2',
+      username: 'raphael',
+      fullName: 'Raffaello Sanzio da Urbino',
+      emailAddress: 'raphael@sanzio.com',
+      following: [],
+      followers: [process.env.REACT_APP_FIREBASE_USER_ID],
+      dateCreated: Date.now()
+    },
+    {
+      userId: '3',
+      username: 'dali',
+      fullName: 'Salvador Dalí',
+      emailAddress: 'salvador@dali.com',
+      following: [],
+      followers: [process.env.REACT_APP_FIREBASE_USER_ID],
+      dateCreated: Date.now()
+    },
+    {
+      userId: '4',
+      username: 'orwell',
+      fullName: 'George Orwell',
+      emailAddress: 'george@orwell.com',
+      following: [],
+      followers: [process.env.REACT_APP_FIREBASE_USER_ID],
+      dateCreated: Date.now()
+    }
+  ];
+
+  // 2-26-21: `for (const user in users)` will not work
+  for (let k = 0; k < users.length; k++) {
+    FirebaseApp.firestore().collection('users').add(users[k]);
+  }
+
+  for (let i = 1; i <= 5; ++i) {
+    FirebaseApp
+      .firestore()
+      .collection('photos')
+      .add({
+        photoId: i,
+        userId: '2',
+        imageSrc: `/images/users/raphael/${i}.jpg`,
+        caption: 'Saint George and the Dragon',
+        likes: [],
+        comments: [
+          {
+            displayName: 'dali',
+            comment: 'Love this place, looks like my animal farm!'
+          },
+          {
+            displayName: 'orwell',
+            comment: 'Would you mind if I used this picture?'
+          }
+        ],
+        userLatitude: '40.7128°',
+        userLongitude: '74.0060°',
+        dateCreated: Date.now()
+      });
+  }
+}
